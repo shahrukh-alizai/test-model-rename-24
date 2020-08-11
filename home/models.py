e06d61101de639994d6f0f7ef3e62d7676113287
Shah Rukh Khan <shah@crowdbotics.com>
2020-08-11 11:28:45 +0000

    Update models

--- home/models.py
+++ home/models.py
@@ -7,7 +7,7 @@ from django.db import models
 
 
 class CustomText(models.Model):
-    title = models.CharField(max_length=150,)
+    title234 = models.CharField(max_length=150, blank=True,)
 
     def __str__(self):
         return self.title
